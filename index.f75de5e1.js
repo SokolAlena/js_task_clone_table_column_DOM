98c05430423f24e3853cd3c775be6de288940c1f
Alena Sokol <alenashpakova27021995@gmail.com>
2024-12-27 16:45:50 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,17 @@
+"use strict";
+const table = document.querySelector("table");
+const trs = table.rows;
+function addColumn(lines) {
+    const rows = [
+        ...lines
+    ];
+    rows.forEach((row)=>{
+        const tr = row.cells;
+        const secondTr = tr[1].cloneNode(true);
+        row.append(secondTr);
+        row.append(tr[4]);
+    });
+}
+addColumn(trs);
+
+//# sourceMappingURL=index.f75de5e1.js.map
